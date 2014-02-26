28be5e4244445071b9656cdf8e87aed817e6b4c1
Mikko Lainio <mlainio@gmail.com>
2014-02-26 14:49:51 +0200

    Changed theme to "Neptune", added some panels to Help view.

--- app/view/Help.js
+++ app/view/Help.js
@@ -4,8 +4,32 @@ Ext.define('RiskiMarket.view.Help', {
     xtype: 'app-help',
 
     layout: {
-        type: 'fit'
+        type: 'table',
+		columns: 1,
+		rows: 10,
+		tdAttrs: { style: 'padding: 10px; ' }
     },
-    
-    html: 'help'
+	defaults: {
+		xtype: 'panel',
+		width: '100%',
+		bodypadding: 10
+	},	
+	items: [
+		{
+			frame: true,
+			title: "Help",
+			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		},
+		{
+			//frame: true,
+			//title: "Help",
+			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		},
+		{
+			//frame: true,
+			//title: "Help",
+			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		}
+	]
+	
 });
