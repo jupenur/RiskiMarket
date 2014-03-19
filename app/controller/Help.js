5324b45c327a6b7e0af684863497bbe419a9b3e6
Mikko Lainio <mlainio@gmail.com>
2014-03-19 18:49:16 +0200

    Helpin kontrollerissa on nyt listener (ei tee mitään), view:n muotoilua.

--- app/controller/Help.js
+++ app/controller/Help.js
@@ -1,3 +1,13 @@
 Ext.define('RiskiMarket.controller.Help', {
-    extend: 'Ext.app.Controller'
+    extend: 'Ext.app.Controller',
+	init: function() {
+		this.control({
+			'*':{
+				changehelpstate: this.changeHelpState
+			}
+		})
+	}
+	changeHelpState: function(state) {
+		
+	}
 });
--- app/view/Help.js
+++ app/view/Help.js
@@ -5,29 +5,49 @@ Ext.define('RiskiMarket.view.Help', {
 
     layout: {
         type: 'table',
-		columns: 1,
+		columns: 2,
 		rows: 10,
-		tdAttrs: { style: 'padding: 10px; ' }
+		tdAttrs: { style: 'padding: 3px; ' }
     },
 	defaults: {
 		xtype: 'panel',
-		width: '100%',
-		bodypadding: 10
+		//width: '100%',
+		layout : 'fit',
+		bodyPadding: 10
 	},	
 	items: [
 		{
+			xtype: 'label',
+			width: 30,
+			html:"<p style = 'font-size:20px'> 1.</p>"			
+		},
+		{
 			frame: true,
 			title: "Help",
+			//width: '80%',
 			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			
+		},
+		{
+			xtype: 'label',
+			width: 30,
+			html:"<p style = 'font-size:20px'> 2.</p>",
 		},
 		{
 			//frame: true,
 			//title: "Help",
+			//width: '80%',
 			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		},
+		{
+			xtype: 'label',
+			width: 30,
+			html:"<p style = 'font-size:20px'> 3.</p>",
 		},
 		{
 			//frame: true,
 			//title: "Help",
+			//width: '80%',
 			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		}
 	]
