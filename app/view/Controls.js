253f1f3f1870df28141e6763a05561419228e000
Juho Nurminen <juho@vaadin.com>
2014-03-19 17:50:59 +0200

    Added some more structure to the controls view

--- app/view/Controls.js
+++ app/view/Controls.js
@@ -1,13 +1,42 @@
 Ext.define('RiskiMarket.view.Controls', {
     extend: 'Ext.container.Container',
-
     xtype: 'app-controls',
 
-    layout: {
-        type: 'card'
+    layout: 'vbox',
+    height: '100%',
+    disabled: true,
+    defaults: {
+        width: '100%'
     },
     
-    items: [{
-        html: 'controls'
-    }]
+    initComponent: function () {
+        this.callParent(arguments);
+        
+        Ext.create('Ext.window.Window', {
+            title: 'Kirjaudu sisään',
+            closable: false,
+            draggable: false,
+            resizable: false,
+            padding: '0 20 0 20',
+            border: false,
+            html: '<p>Lue avainkortti kirjautuaksesi sisään ja tehdäksesi ostoksia.</p>',
+            height: 100,
+            width: 250
+        }).show();
+    },
+    
+    items: [
+        {
+            html: 'product info',
+            height: 100
+        },
+        {
+            html: 'cart',
+            flex: 1
+        },
+        {
+            html: 'total',
+            height: 100
+        }
+    ]
 });
