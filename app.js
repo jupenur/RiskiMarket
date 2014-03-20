f2ea2d36fbf070f7792ef450e21332347b0445c7
Juho Nurminen <juho@vaadin.com>
2014-03-21 00:54:22 +0200

    Added a method for generating dummy test data

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ Ext.application({
     name: 'RiskiMarket',
 
     extend: 'RiskiMarket.Application',
-    
+
     autoCreateViewport: true
 });
+
+function generateTestData() {
+    localStorage.clear();
+
+    RiskiMarket.app.getStore('Products').add(
+        { key: '1234', name: 'Hapansilakka, 1 kg', price: 39.95 },
+        { key: '5678', name: 'Turducken',          price:  2.50 },
+        { key: '9012', name: 'Mämmiroppinen',      price: 13.37 },
+        { key: '3456', name: 'Piimä, 1 l',         price:   225 },
+        { key: '7890', name: 'Kengänpohja, 2 kpl', price:  5.00 }
+    );
+
+    RiskiMarket.app.getStore('Users').add(
+        { key: 'qwerty', name: 'Juho Nurminen', balance:  85.72 },
+        { key: 'asdfgh', name: 'Mikko Lainio',  balance: 132.05 },
+        { key: 'zxcvbn', name: 'Eero Pihkala',  balance:  97.99 }
+    );
+}
