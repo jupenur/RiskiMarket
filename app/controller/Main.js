f6c3f1a2a72a86745d8bdc93ce6b2ebfd3dcef73
Juho Nurminen <juho@vaadin.com>
2014-03-20 19:50:37 +0200

    Explicitly instantiate the child controllers of Main

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -1,16 +1,29 @@
 Ext.define('RiskiMarket.controller.Main', {
     extend: 'Ext.app.Controller',
-    
+
     views: [ 'Main' ],
-    
+    requires: [
+        'RiskiMarket.controller.Controls',
+        'RiskiMarket.controller.Help',
+        'RiskiMarket.controller.Products',
+    ],
+
     statics: {
         InputMode: {
             TEXT: 0,
             ID: 1
         }
     },
-    
+
+    inputMode: undefined,
+
     init: function () {
+        // child controllers
+        this.getController('Controls');
+        this.getController('Help');
+        this.getController('Products');
+
+        // input modes
         this.inputMode = RiskiMarket.controller.Main.InputMode.ID;
         var inputBuffer = '', controller = this;
         document.documentElement.addEventListener('keypress', function (e) {
