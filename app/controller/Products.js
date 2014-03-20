c6b3ee65f038021603df2101aae3018cfda5612e
Juho Nurminen <juho@vaadin.com>
2014-03-21 01:24:32 +0200

    Added product selection logic

--- app/controller/Products.js
+++ app/controller/Products.js
@@ -1,5 +1,33 @@
 Ext.define('RiskiMarket.controller.Products', {
     extend: 'Ext.app.Controller',
 
-    stores: [ 'Products' ]
+    stores: [ 'Products' ],
+    refs: [{ ref: 'productsGrid', selector: 'app-products' }],
+
+    init: function () {
+        this.listen({
+            controller: {
+                '*': {
+                    input: function (input) {
+                        var product = this.getProductsStore()
+                                          .findRecord('key', input);
+                        if (product != null) {
+                            this.getProductsGrid()
+                                .getSelectionModel()
+                                .select(product);
+                            return false;
+                        }
+                    }
+                }
+            }
+        });
+
+        this.control({
+            'app-products': {
+                select: function (_, product) {
+                    this.fireEvent('product', product);
+                }
+            }
+        });
+    }
 });
