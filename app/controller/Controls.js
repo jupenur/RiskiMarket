1b5379c5602e533514fb3bd963c9cd1347f6a74d
Juho Nurminen <juho@vaadin.com>
2014-03-20 22:53:25 +0200

    Added login event handling to the Controls controller

--- app/controller/Controls.js
+++ app/controller/Controls.js
@@ -1,3 +1,23 @@
 Ext.define('RiskiMarket.controller.Controls', {
-    extend: 'Ext.app.Controller'
+    extend: 'Ext.app.Controller',
+
+    refs: [
+        { ref: 'controls', selector: 'app-controls' }
+    ],
+
+    init: function () {
+        this.listen({
+            controller: {
+                '*': {
+                    login: function (user) {
+                        this.getControls().enable();
+                    },
+
+                    logout: function (user) {
+                        this.getControls().disable();
+                    }
+                }
+            }
+        });
+    }
 });
