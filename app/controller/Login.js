9c09c00a0a5b6aa659abb77905ef010764e50ed7
Juho Nurminen <juho@vaadin.com>
2014-03-20 22:39:52 +0200

    Added login logic

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -3,9 +3,40 @@ Ext.define('RiskiMarket.controller.Login', {
 
     views: [ 'Login' ],
     models: [ 'User' ],
+    stores: [ 'Users' ],
+
+    user: null,
 
     init: function () {
         this.view = this.getLoginView().create();
         this.view.show();
+
+        this.listen({
+            controller: {
+                '*': {
+                    input: function (input) {
+                        if (this.user === null) {
+                            this.user = this.getUsersStore().findRecord('key', input);
+                            if (this.user !== null) {
+                                this.fireEvent('login', this.user);
+                                return false;
+                            }
+                        } else if (input === this.user.get('key')) {
+                            this.fireEvent('logout', this.user);
+                            this.user = null;
+                            return false;
+                        }
+                    },
+
+                    login: function (user) {
+                        this.view.hide();
+                    },
+
+                    logout: function (user) {
+                        this.view.show();
+                    }
+                }
+            }
+        });
     }
 });
