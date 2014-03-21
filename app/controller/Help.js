ea38bf42ba653c07adce4a2512a3d7b0987504c4
Mikko Lainio <mlainio@gmail.com>
2014-03-21 21:31:34 +0200

    Help section reacts to events. and selects appropriate help section (login implemented)

--- app/controller/Help.js
+++ app/controller/Help.js
@@ -1,13 +1,26 @@
 Ext.define('RiskiMarket.controller.Help', {
     extend: 'Ext.app.Controller',
-    init: function() {
+    
+	refs: [
+		{ ref: 'helpview', selector: 'app-help' }
+	],
+	
+	init: function() {
+		this.listen({
+			controller: {
+				'*' : {
+					login: function() { this.changeHelpState(1);}
+				}
+			}
+		});
         this.control({
             '*':{
-                changehelpstate: this.changeHelpState
+                
             }
-        })
+        });
+		//this.changeHelpState(1);
     },
     changeHelpState: function(state) {
-
+		this.getHelpview().changeSelection(state);
     }
 });
--- app/view/Help.js
+++ app/view/Help.js
@@ -2,54 +2,56 @@ Ext.define('RiskiMarket.view.Help', {
     extend: 'Ext.container.Container',
 
     xtype: 'app-help',
+	layout: {
+		type: 'table',
+		columns: 1,
+		rows: 40
+	},
+	
+	requires: ['RiskiMarket.View.help.HelpSection'],
 
-    layout: {
+/*     layout: {
         type: 'table',
-		columns: 2,
+		columns: 1,
 		rows: 10,
 		tdAttrs: { style: 'padding: 3px; ' }
-    },
+    }, */
 	defaults: {
 		xtype: 'panel',
 		//width: '100%',
-		layout : 'fit',
+		width: 200,
+		//layout : 'fit',
 		bodyPadding: 10
 	},	
 	items: [
 		{
-			xtype: 'label',
-			width: 30,
-			html:"<p style = 'font-size:20px'> 1.</p>"			
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		},
 		{
-			frame: true,
-			title: "Help",
-			//width: '80%',
-			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-			
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		},
 		{
-			xtype: 'label',
-			width: 30,
-			html:"<p style = 'font-size:20px'> 2.</p>",
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		},
 		{
-			//frame: true,
-			//title: "Help",
-			//width: '80%',
-			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		},
 		{
-			xtype: 'label',
-			width: 30,
-			html:"<p style = 'font-size:20px'> 3.</p>",
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		},
 		{
-			//frame: true,
-			//title: "Help",
-			//width: '80%',
-			html: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			xtype: 'app-helpsection',			
+			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 		}
-	]
+
+	],
+	changeSelection : function(value) {
+		this.getComponent(value-1).selected();
+	}
 	
 });
