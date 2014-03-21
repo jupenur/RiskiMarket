c9e36600d7cac4d68ddbad1b505e39cbb2d85408
Juho Nurminen <juho@vaadin.com>
2014-03-21 21:51:56 +0200

    Corrected package names

--- app/view/Help.js
+++ app/view/Help.js
@@ -2,56 +2,56 @@ Ext.define('RiskiMarket.view.Help', {
     extend: 'Ext.container.Container',
 
     xtype: 'app-help',
-	layout: {
-		type: 'table',
-		columns: 1,
-		rows: 40
-	},
-	
-	requires: ['RiskiMarket.View.help.HelpSection'],
+    layout: {
+        type: 'table',
+        columns: 1,
+        rows: 40
+    },
+
+    requires: ['RiskiMarket.view.help.HelpSection'],
 
 /*     layout: {
         type: 'table',
-		columns: 1,
-		rows: 10,
-		tdAttrs: { style: 'padding: 3px; ' }
+        columns: 1,
+        rows: 10,
+        tdAttrs: { style: 'padding: 3px; ' }
     }, */
-	defaults: {
-		xtype: 'panel',
-		//width: '100%',
-		width: 200,
-		//layout : 'fit',
-		bodyPadding: 10
-	},	
-	items: [
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		},
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		},
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		},
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		},
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		},
-		{
-			xtype: 'app-helpsection',			
-			helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		}
+    defaults: {
+        xtype: 'panel',
+        //width: '100%',
+        width: 200,
+        //layout : 'fit',
+        bodyPadding: 10
+    },
+    items: [
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+            xtype: 'app-helpsection',
+            helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        }
 
-	],
-	changeSelection : function(value) {
-		this.getComponent(value-1).selected();
-	}
-	
+    ],
+    changeSelection : function(value) {
+        this.getComponent(value-1).selected();
+    }
+
 });
