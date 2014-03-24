4adb2aa4c9d1398a2e8e17f3d8df90ed831587b5
Juho Nurminen <juho@vaadin.com>
2014-03-24 16:04:13 +0200

    Fixed the bugs in cart updating

--- app/controller/Controls.js
+++ app/controller/Controls.js
@@ -26,10 +26,10 @@ Ext.define('RiskiMarket.controller.Controls', {
             ref: 'balanceField',
             selector: 'app-controls > form > displayfield[name=balance]'
         },
-		{
-			ref: 'helloPanel',
-			selector: 'app-controls > #hello'
-		}
+        {
+            ref: 'helloPanel',
+            selector: 'app-controls > #hello'
+        }
     ],
 
     user: null,
@@ -71,47 +71,47 @@ Ext.define('RiskiMarket.controller.Controls', {
                         this.getEditHint().setVisible(user.get('admin'));
                         this.getBalanceField().setValue(user.get('balance'));
                         this.getControls().enable();
-						this.setUserGreeting(user);						
+                        this.setUserGreeting(user);
                     },
 
                     logout: function (user) {
-                        var sum = parseFloat(this.getSumField().getValue());						
-                        if (!isNaN(sum)) {                           
-						    if ( user.get('balance') >= sum ) {
-								user.set('balance', user.get('balance') - sum);
-								Ext.Msg.show({
-									title: 'Ostokset veloitettu',
-									msg: 'Ostokset on veloitettu tililtäsi. '
-										+ 'Saldo: '+ user.get('balance').toFixed(2) + ' €<br> '
-										+ 'Sinut on kirjattu ulos automaattisesti',
-									icon: Ext.Msg.INFO,
-									closable: false,
-									modal: false
-								}).setPosition(undefined, 20);
-								setTimeout(function () {
-									Ext.Msg.hide();
-								}, 5000);
-							}
-							else {
-								Ext.Msg.show({
-									title: 'Saldo ei riitä!',
-									msg: 'Ostoksia ei veloitettu tililtäsi, koska tilin saldo ei riitä.<br>' 				
-										+ 'Sinut on kirjattu ulos automaattisesti',
-									icon: Ext.Msg.INFO,
-									closable: false,
-									modal: false
-								}).setPosition(undefined, 20);
-								setTimeout(function () {
-									Ext.Msg.hide();
-								}, 5000);
-							}
+                        var sum = parseFloat(this.getSumField().getValue());
+                        if (!isNaN(sum)) {
+                            if ( user.get('balance') >= sum ) {
+                                user.set('balance', user.get('balance') - sum);
+                                Ext.Msg.show({
+                                    title: 'Ostokset veloitettu',
+                                    msg: 'Ostokset on veloitettu tililtäsi. '
+                                        + 'Saldo: '+ user.get('balance').toFixed(2) + ' Eur<br> '
+                                        + 'Sinut on kirjattu ulos automaattisesti',
+                                    icon: Ext.Msg.INFO,
+                                    closable: false,
+                                    modal: false
+                                }).setPosition(undefined, 20);
+                                setTimeout(function () {
+                                    Ext.Msg.hide();
+                                }, 5000);
+                            }
+                            else {
+                                Ext.Msg.show({
+                                    title: 'Saldo ei riitä!',
+                                    msg: 'Ostoksia ei veloitettu tililtäsi, koska tilin saldo ei riitä.<br>'
+                                        + 'Sinut on kirjattu ulos automaattisesti',
+                                    icon: Ext.Msg.INFO,
+                                    closable: false,
+                                    modal: false
+                                }).setPosition(undefined, 20);
+                                setTimeout(function () {
+                                    Ext.Msg.hide();
+                                }, 5000);
+                            }
                         }
                         this.user = null;
                         this.getEditHint().setVisible(false);
                         this.getBalanceField().setValue('--');
                         this.getCart().getStore().removeAll();
                         this.getControls().disable();
-						this.setUserGreeting(null);
+                        this.setUserGreeting(null);
                     },
 
                     product: function (product) {
@@ -139,38 +139,38 @@ Ext.define('RiskiMarket.controller.Controls', {
                             this.removeProduct();
                             return false;
                         }
-						if (key === 45 && this.user && this.user.get('admin')) {	//Insert
-							this.insertProduct();
-							return false
-						
-						}
                     }
                 }
             },
             store: {
                 '#Products' : {
-                    datachanged : function (store) {                        
-                        console.log("data changed");
+                    datachanged: function (store) {
                         var cart = this.getCart().getStore();
-                        var info = this.getProductInfo().getForm().getRecord();
-                        console.log(info);
-                        var match;
                         cart.each(function (rec) {
-                            match = store.findRecord('key',rec.get('key'));
+                            var match = store.findRecord(
+                                'key',rec.get('key'), 0, false, true, true);
                             if (match != null) {
-                                rec.set('name',match.get('name'));
-                                rec.set('price',match.get('price'));
+                                rec.set('name', match.get('name'));
+                                rec.set('price', match.get('price'));
+                            } else {
+                                cart.remove(rec);
                             }
                         });
-                        match = store.findRecord('key',info.get('key'));
-                        console.log(match);                        
-                        if (match != null)
-                            this.getProductInfo().loadRecord(match);
-                        
+
+                        var info = this.getProductInfo().getForm().getRecord();
+                        if (info) {
+                            info = store.findRecord(
+                                'key',info.get('key'), 0, false, true, true);
+                            if (info) {
+                                this.getProductInfo().loadRecord(info);
+                            } else {
+                                this.getCart().getSelectionModel().select(
+                                    this.getProductsStore().count() - 1);
+                            }
+                        }
                     }
                 }
             }
-            
         });
     },
 
@@ -205,31 +205,26 @@ Ext.define('RiskiMarket.controller.Controls', {
                           .select(this.getProductsStore().count() - 1);
         }
     },
-	
-	insertProduct: function () {
-		console.log("Inserting product");
-		return false;
-	},
-	
-	setUserGreeting: function(user) {
-		var username = ((user != null) ? user.get('name') : null);
-		var helloStart = "<h2 align='center'>";
-		var	helloUserStart = 'Hei ';
-		var	helloUserEnd = '! ';
-		var	helloEnd = 'Tervetuloa RiskiMarketiin!</h2>';
-		var panel = this.getHelloPanel()
-		var string;
-		
-		console.log(Ext.getDisplayName(panel));
-									
-		if (user != null)		
-			string = helloStart+helloUserStart+username+helloUserEnd+helloEnd;
-		else
-			string = helloStart+helloEnd;
-		console.log(string);
-		panel.update(string);
-		this.getControls().setGreetBackground(user);
-		
-		
-	}
+
+    setUserGreeting: function(user) {
+        var username = ((user != null) ? user.get('name') : null);
+        var helloStart = "<h2 align='center'>";
+        var helloUserStart = 'Hei ';
+        var helloUserEnd = '! ';
+        var helloEnd = 'Tervetuloa RiskiMarketiin!</h2>';
+        var panel = this.getHelloPanel()
+        var string;
+
+        if (user != null) {
+            string = helloStart
+                + helloUserStart
+                + username
+                + helloUserEnd
+                + helloEnd;
+        } else {
+            string = helloStart + helloEnd;
+        }
+        panel.update(string);
+        this.getControls().setGreetBackground(user);
+    }
 });
